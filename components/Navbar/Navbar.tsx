106e6752032f6362ea7cc4409b52a5830c8d7f3a
JyotiprasadBorgohain <jyotiprasad.borgohain24@gmail.com>
2026-08-09 03:16:58 +0530

    profile pic issue solved

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
                 <a href="#home" className={styles.logo}>
 
                     <Image
-                        src="/public/images/profile.png"
+                        src="public/images/profile.png"
                         alt="Jyotiprasad"
                         width={52}
                         height={52}
